00eb8f1e5d185fb91a8de07d2836fc64cdea41ed
何俊林 <3241318570@qq.com>
2026-03-18 14:34:45 +0800

    后端项目初始化

---
+++ src/main/java/com/example/airecruitmentbackend/AiRecruitmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.airecruitmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecruitmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRecruitmentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airecruitmentbackend/AiRecruitmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airecruitmentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecruitmentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
